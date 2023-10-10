fa45e2eccb5ae377d0324706f9c8d75a9129c625
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-10-10 16:02:26 +0530

    detect and aim at enemy done

---
+++ SimplyShooterTest/Assets/Scripts/Player/MVC/PlayerModel.cs
@@ -0,0 +1,6 @@
+
+
+public class PlayerModel 
+{
+    public EnemyView Enemy;
+}
--- SimplyShooterTest/Assets/Scripts/Services/EventService.cs
+++ SimplyShooterTest/Assets/Scripts/Services/EventService.cs
@@ -1,6 +1,5 @@
 
 using System;
-using UnityEngine;
 
 public class EventService : GenericSingleton<EventService>
 {
