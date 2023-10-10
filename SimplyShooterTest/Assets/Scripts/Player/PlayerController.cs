a10aa1f8c50c6416a673253b5a1221fe47072eee
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-10-10 14:34:37 +0530

    Remove Touuch handeler from player view

--- SimplyShooterTest/Assets/Scripts/Player/PlayerController.cs
+++ SimplyShooterTest/Assets/Scripts/Player/PlayerController.cs
@@ -9,36 +9,7 @@ public class PlayerController
     {
         playerView = view;
     }
-    public void SpawnJoystick()
-    {
-        movementAmount = Vector2.zero;
-        playerView.MovementJoystick.gameObject.SetActive(true);
-        playerView.MovementJoystick.JoystickRectTransform.anchoredPosition = ClampPostion(playerView.MovementFinger.screenPosition, playerView.MovementJoystick);
-    }
-
-    private Vector2 ClampPostion(Vector2 startPosition, JoystickController joystick)
-    {
-        if (startPosition.x < joystick.JoystickSize.x / 2)
-        {
-            startPosition.x = joystick.JoystickSize.x / 2;
-
-        }
-        else if (Screen.width - startPosition.x < joystick.JoystickSize.x / 2)
-        {
-            startPosition.x = Screen.width - joystick.JoystickSize.x / 2;
-        }
-        if(startPosition.y < joystick.JoystickSize.y / 2)
-        {
-            startPosition.y = joystick.JoystickSize.y / 2;
-
-        }
-        else if (Screen.height - startPosition.y < joystick.JoystickSize.y / 2)
-        {
-            startPosition.y = Screen.width - joystick.JoystickSize.y / 2;
-        }
-        return startPosition;
-    }
-
+    
     public void SetMovementAmount(Vector2 movAmt)
     {
         movementAmount = movAmt;
