bc04be6181a917e56944a7c166f13e6509c806f0
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-10-10 12:02:07 +0530

    Minor changes

--- SimplyShooterTest/Assets/Scripts/Joystick/JoystickController.cs
+++ SimplyShooterTest/Assets/Scripts/Joystick/JoystickController.cs
@@ -4,5 +4,12 @@ using UnityEngine.UI;
 
 public class JoystickController : MonoBehaviour
 {
+    [HideInInspector]
+    public RectTransform JoystickRectTransform;
     public RawImage JoystickKnob;
+
+    private void Awake()
+    {
+        JoystickRectTransform  = GetComponent<RectTransform>();
+    }
 }
